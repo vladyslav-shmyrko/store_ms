57dbfaf6234a3fc5eff51e720ed00be39a18fbb4
Vladyslav Shmyrko <vladyslav0496@gmail.com>
2023-11-15 21:43:00 +0200

    add products-service scripts to run linters

--- products-service/src/index.js
+++ products-service/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from Products' });
 });
 
